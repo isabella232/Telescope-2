477ebc6bbfec1645419abf8b1c5250c520b2ea79
Charlie DeTar <cfd@media.mit.edu>
2015-02-02 15:31:01 -0700

    Support chromedriver with nightwatch

--- run_nightwatch.sh
+++ run_nightwatch.sh
@@ -25,5 +25,17 @@ if [ ! -e  $SELENIUM_JAR ] ; then
   curl -L "https://selenium-release.storage.googleapis.com/2.43/selenium-server-standalone-2.43.1.jar" > $SELENIUM_JAR
 fi
 
+CHROME_DRIVER="$DIR/tests/nightwatch/.bin/chromedriver"
+if [ ! -e $CHROME_DRIVER ] ; then
+  if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    ARCH="linux64"
+  elif [[ "$unamestr" == "darwin"* ]]; then
+    ARCH="mac32"
+  fi
+  curl -L "https://chromedriver.storage.googleapis.com/2.14/chromedriver_${ARCH}.zip" > ${CHROME_DRIVER}.zip
+  unzip ${CHROME_DRIVER}.zip -d `dirname $CHROME_DRIVER`
+  rm ${CHROME_DRIVER}.zip
+fi
+
 cd $DIR
 NODE_PATH=$NPM_BASE/node_modules $NIGHTWATCH -c $DIR/tests/nightwatch_config.json "$@"
