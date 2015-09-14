cef80f558dbb93e021e83ad54f902ece5ae60f12
Charlie DeTar <cfd@media.mit.edu>
2015-09-14 16:01:19 -0600

    Fix post display

--- packages/admithub-telescope-theme/lib/client/views/post_item.js
+++ packages/admithub-telescope-theme/lib/client/views/post_item.js
@@ -2,7 +2,7 @@ Template.ah_post_title_and_content.helpers({
   getBodyTemplateName: function() {
     // Return the full body if we're on a post detail page, and a teaser
     // otherwise.
-    if (_.contains(["post_page_with_slug", "post_page"], Router.current().route.getName())) {
+    if (_.contains(["post_page"], Router.current().route.getName())) {
       return "post_body";
     }
     return "ah_post_teaser";
